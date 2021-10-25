5e2e7e252f6b224cc1347cc3944b8b90ce3e676d
Bin Lama <50900779+BinLama@users.noreply.github.com>
2021-10-24 18:57:12 -0700

    deploy

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict'
+
+let tracitivity = document.getElementById("tractivity");
+
+
+tracitivity.addEventListener("click", tracitivity_link_opener);
+
+function tracitivity_link_opener() {
+    window.open("https://replit.com/@BinLama/fitnesstracker#index.js", "_blank");
+}
