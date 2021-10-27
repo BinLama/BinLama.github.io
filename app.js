8d941f176b48e6320c4505ab5a8ecc43190a0c13
Bin Lama <50900779+BinLama@users.noreply.github.com>
2021-10-27 14:25:05 -0700

    links colors change

--- app.js
+++ app.js
@@ -1,10 +1,78 @@
 'use strict'
 
 let tracitivity = document.getElementById("tractivity");
+let about = document.getElementById("li_about");
+let skills = document.getElementById("li_skills");
+let project = document.getElementById("li_project");
+let edu = document.getElementById("li_edu");
+let contact = document.getElementById("li_contact");
+
+// currently selected value
+let selected = about;
 
 
 tracitivity.addEventListener("click", tracitivity_link_opener);
 
 function tracitivity_link_opener() {
     window.open("https://replit.com/@BinLama/fitnesstracker#index.js", "_blank");
+}
+
+
+
+// adding event listeners for scroll function
+about.addEventListener("click", aboutChangeClass);
+skills.addEventListener("click", skillsChangeClass);
+project.addEventListener("click", projectChangeClass);
+edu.addEventListener("click", eduChangeClass);
+contact.addEventListener("click", contactChangeClass);
+
+// change class based on click for about
+function aboutChangeClass() {
+    about.classList.add("current_page");
+    if (selected != about) {
+        selected.classList.remove("current_page")
+        selected = about; 
+    }
+    console.log(about.classList);
+}
+
+// change class based on click for skills
+function skillsChangeClass() {
+    skills.classList.add("current_page");
+    console.log(selected);
+    if (selected != skills) {
+        selected.classList.remove("current_page")
+        selected = skills; 
+    }
+    console.log(skills.classList);
+}
+
+// change class based on click for about
+function projectChangeClass() {
+    project.classList.add("current_page");
+    if (selected != project) {
+        selected.classList.remove("current_page")
+        selected = project; 
+    }
+    console.log(project.classList);
+}
+
+// change class based on click for edu
+function eduChangeClass() {
+    edu.classList.add("current_page");
+    if (selected != edu) {
+        selected.classList.remove("current_page")
+        selected = edu; 
+    }
+    console.log(edu.classList);
+}
+
+// change class based on click for contact
+function contactChangeClass() {
+    contact.classList.add("current_page");
+    if (selected != contact) {
+        selected.classList.remove("current_page")
+        selected = contact; 
+    }
+    console.log(contact.classList);
 }
